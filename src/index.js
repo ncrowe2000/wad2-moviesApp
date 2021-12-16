49c26a1845878a5c548ddaab3b9dad709a5eeadd
Niall Crowe <20089334@mail.wit.ie>
2021-12-16 12:31:18 +0000

    Added Upcoming Movies page.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviesPage from './pages/upcomingMoviesPage';
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <BrowserRouter>
           <SiteHeader />      {/* New Header  */}
         <Switch>
+          <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
