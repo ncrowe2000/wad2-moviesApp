3d08a744910b13d53bb3e7904dfdacb9f6f7f163
Niall Crowe <20089334@mail.wit.ie>
2022-08-27 17:23:46 +0100

    Fixed storybook support for movieListHeader.stories.js

--- src/stories/movieListHeader.stories.js
+++ src/stories/movieListHeader.stories.js
@@ -1,9 +1,13 @@
 import React from "react";
 import MovieListHeader from "../components/headerMovieList";
+import { MemoryRouter } from "react-router";
 
 export default {
   title: "Home Page/Header",
   component: MovieListHeader,
+  decorators: [
+    (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
+  ]
 };
 
 export const Basic = () => <MovieListHeader title={'Discover Movies'} />;
